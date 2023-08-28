842c25b7f18b4d94d371850ca7233779dc32cf53
Jiri Mosinger <jiri.mosinger@broadcom.com>
2023-08-28 09:51:54 +0200

    Refactor

--- clients/vscode-hlasmplugin/syntaxes/scripts/grammarGenerator.ts
+++ clients/vscode-hlasmplugin/syntaxes/scripts/grammarGenerator.ts
@@ -2,6 +2,11 @@ var fs = require('fs')
 
 const grammar_template = './syntaxes/scripts/hlasm_grammar_template.txt';
 
+const code_block_listing_begin = '.{2}Loc  Object Code    Addr1 Addr2  Stmt   Source Statement.*';
+const code_block_listing_long_begin = '.{2}Loc    Object Code      Addr1    Addr2    Stmt  Source Statement.*';
+const code_block_listing_annotation_length = '.{40}';
+const code_block_listing_long_annotation_length = '.{49}';
+
 enum GrammarTypes {
   Main = 1,
   ListingLong,
@@ -16,11 +21,8 @@ interface GrammarProperties {
   scope: string;
   entryPattern: string;
   codeBlockBegin: string;
-  codeBlockEnd: string;
   beginLineSkipRule: string;
-  endLineRule: string;
-  hlasmLineContent: string;
-  beginAsmaRule: string;
+  listingOffset: string;
 }
 
 const hlasmGrammar: GrammarProperties = {
@@ -29,11 +31,8 @@ const hlasmGrammar: GrammarProperties = {
   scope: 'hlasm',
   entryPattern: 'hlasm_syntax',
   codeBlockBegin: '',
-  codeBlockEnd: '',
-  beginLineSkipRule: '^',
-  endLineRule: '((?<=^.{71}\\\\s.*)|(?<=^.{0,71}))$',
-  hlasmLineContent: '^.{0,71}',
-  beginAsmaRule: '^\\\\*\\\\*'
+  beginLineSkipRule: '',
+  listingOffset: ''
 }
 
 const hlasmListingGrammar: GrammarProperties = {
@@ -41,12 +40,9 @@ const hlasmListingGrammar: GrammarProperties = {
   grammarName: 'IBM HLASM Listing',
   scope: 'hlasmListing',
   entryPattern: 'code_block',
-  codeBlockBegin: '^.{2}Loc  Object Code    Addr1 Addr2  Stmt   Source Statement.*',
-  codeBlockEnd: '^                                                 Relocation Dictionary|                                          Macro and Copy Code Source Summary',
-  beginLineSkipRule: '^.{40}',
-  endLineRule: '((?<=^.{40}.{71}\\\\s.*)|(?<=^.{40}.{0,71}))$',
-  hlasmLineContent: '^.{40}.{0,71}',
-  beginAsmaRule: '^\\\\*\\\\*'
+  codeBlockBegin: code_block_listing_begin,
+  beginLineSkipRule: code_block_listing_annotation_length,
+  listingOffset: ''
 }
 
 const hlasmListingLongGrammar: GrammarProperties = {
@@ -54,12 +50,9 @@ const hlasmListingLongGrammar: GrammarProperties = {
   grammarName: 'IBM HLASM Listing Long',
   scope: 'hlasmListingLong',
   entryPattern: 'code_block',
-  codeBlockBegin: '^.{2}Loc    Object Code      Addr1    Addr2    Stmt  Source Statement.*',
-  codeBlockEnd: '^                                                 Relocation Dictionary',
-  beginLineSkipRule: '^.{49}',
-  endLineRule: '((?<=^.{49}.{71}\\\\s.*)|(?<=^.{49}.{0,71}))$',
-  hlasmLineContent: '^.{49}.{0,71}',
-  beginAsmaRule: '^\\\\*\\\\*'
+  codeBlockBegin: code_block_listing_long_begin,
+  beginLineSkipRule: code_block_listing_long_annotation_length,
+  listingOffset: ''
 }
 
 const hlasmListingEndevorGrammar: GrammarProperties = {
@@ -67,12 +60,9 @@ const hlasmListingEndevorGrammar: GrammarProperties = {
   grammarName: 'IBM HLASM Listing Endevor',
   scope: 'hlasmListingEndevor',
   entryPattern: 'code_block',
-  codeBlockBegin: '^.{3}Loc  Object Code    Addr1 Addr2  Stmt   Source Statement.*',
-  codeBlockEnd: '^(1.{8})                                         Relocation Dictionary',
-  beginLineSkipRule: '^.{41}',
-  endLineRule: '((?<=^.{41}.{71}\\\\s.*)|(?<=^.{41}.{0,71}))$',
-  hlasmLineContent: '^.{41}.{0,71}',
-  beginAsmaRule: '^.\\\\*\\\\*'
+  codeBlockBegin: code_block_listing_begin,
+  beginLineSkipRule: code_block_listing_annotation_length,
+  listingOffset: '.'
 }
 
 const hlasmListingEndevorLongGrammar: GrammarProperties = {
@@ -80,15 +70,14 @@ const hlasmListingEndevorLongGrammar: GrammarProperties = {
   grammarName: 'IBM HLASM Listing Endevor Long',
   scope: 'hlasmListingEndevorLong',
   entryPattern: 'code_block',
-  codeBlockBegin: '^.{3}Loc    Object Code      Addr1    Addr2    Stmt  Source Statement.*',
-  codeBlockEnd: '^(1.{8})                                         Relocation Dictionary',
-  beginLineSkipRule: '^.{50}',
-  endLineRule: '((?<=^.{50}.{71}\\\\s.*)|(?<=^.{50}.{0,71}))$',
-  hlasmLineContent: '^.{50}.{0,71}',
-  beginAsmaRule: '^.\\\\*\\\\*'
+  codeBlockBegin: code_block_listing_long_begin,
+  beginLineSkipRule: code_block_listing_long_annotation_length,
+  listingOffset: '.'
 }
 
 function generate(props: GrammarProperties) {
+  const listingDetails = props.listingOffset + props.beginLineSkipRule;
+
   fs.readFile(grammar_template, 'utf8', (err: Error, data: string) => {
     if (err) {
       return console.log(err);
@@ -98,11 +87,8 @@ function generate(props: GrammarProperties) {
     result = result.replaceAll('${scope}$', props.scope);
     result = result.replaceAll('${entryPattern}$', props.entryPattern);
     result = result.replaceAll('${codeBlockBegin}$', props.codeBlockBegin);
-    result = result.replaceAll('${codeBlockEnd}$', props.codeBlockEnd);
-    result = result.replaceAll('${endLineRule}$', props.endLineRule);
-    result = result.replaceAll('${beginLineSkipRule}$', props.beginLineSkipRule);
-    result = result.replaceAll('${hlasmLineContent}$', props.hlasmLineContent);
-    result = result.replaceAll('${beginAsmaRule}$', props.beginAsmaRule);
+    result = result.replaceAll('${listingOffset}$', props.listingOffset);
+    result = result.replaceAll('${listingDetails}$', listingDetails);
 
     fs.writeFile(props.grammarDestination, result, 'utf8', (err: any) => {
       if (err) return console.log(err);
