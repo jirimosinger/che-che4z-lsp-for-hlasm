98b4b4eb50a5162605c3ea6fed21aefc0dc7754a
Jiri Mosinger <jiri.mosinger@broadcom.com>
2023-08-28 12:24:01 +0200

    Grammar base

--- clients/vscode-hlasmplugin/syntaxes/scripts/grammarGenerator.ts
+++ clients/vscode-hlasmplugin/syntaxes/scripts/grammarGenerator.ts
@@ -1,6 +1,7 @@
 var fs = require('fs')
 
-const grammar_template = './syntaxes/scripts/hlasm_grammar_template.txt';
+const grammar_details_template = './syntaxes/scripts/hlasm_grammar_template.txt';
+const grammar_base_template = './syntaxes/scripts/hlasm_base_template.txt';
 
 const code_block_listing_begin = '.{2}Loc  Object Code    Addr1 Addr2  Stmt   Source Statement.*';
 const code_block_listing_long_begin = '.{2}Loc    Object Code      Addr1    Addr2    Stmt  Source Statement.*';
@@ -15,48 +16,51 @@ enum GrammarTypes {
   ListingEndevor
 }
 
-interface GrammarProperties {
-  grammarDestination: string;
+interface GrammarFile {
+  file: string;
   grammarName: string;
   scope: string;
+}
+
+interface GrammarDetails extends GrammarFile {
   entryPattern: string;
   codeBlockBegin: string;
   beginLineSkipRule: string;
   listingOffset: string;
 }
 
-const hlasmGrammar: GrammarProperties = {
-  grammarDestination: './syntaxes/hlasm.tmLanguage.json',
-  grammarName: 'IBM HLASM',
-  scope: 'hlasm',
+const hlasmGeneralGrammar: GrammarDetails = {
+  file: './syntaxes/hlasmGeneral.tmLanguage.json',
+  grammarName: 'IBM HLASM General',
+  scope: 'hlasmGeneral',
   entryPattern: 'hlasm_syntax',
   codeBlockBegin: '',
   beginLineSkipRule: '',
   listingOffset: ''
 }
 
-const hlasmListingGrammar: GrammarProperties = {
-  grammarDestination: './syntaxes/hlasmListing.tmLanguage.json',
-  grammarName: 'IBM HLASM Listing',
-  scope: 'hlasmListing',
+const hlasmListingGeneralGrammar: GrammarDetails = {
+  file: './syntaxes/hlasmListingGeneral.tmLanguage.json',
+  grammarName: 'IBM HLASM Listing General',
+  scope: 'hlasmListingGeneral',
   entryPattern: 'code_block',
   codeBlockBegin: code_block_listing_begin,
   beginLineSkipRule: code_block_listing_annotation_length,
   listingOffset: ''
 }
 
-const hlasmListingLongGrammar: GrammarProperties = {
-  grammarDestination: './syntaxes/hlasmListingLong.tmLanguage.json',
-  grammarName: 'IBM HLASM Listing Long',
-  scope: 'hlasmListingLong',
+const hlasmListingGeneralLongGrammar: GrammarDetails = {
+  file: './syntaxes/hlasmListingGeneralLong.tmLanguage.json',
+  grammarName: 'IBM HLASM Listing General Long',
+  scope: 'hlasmListingGeneralLong',
   entryPattern: 'code_block',
   codeBlockBegin: code_block_listing_long_begin,
   beginLineSkipRule: code_block_listing_long_annotation_length,
   listingOffset: ''
 }
 
-const hlasmListingEndevorGrammar: GrammarProperties = {
-  grammarDestination: './syntaxes/hlasmListingEndevor.tmLanguage.json',
+const hlasmListingEndevorGrammar: GrammarDetails = {
+  file: './syntaxes/hlasmListingEndevor.tmLanguage.json',
   grammarName: 'IBM HLASM Listing Endevor',
   scope: 'hlasmListingEndevor',
   entryPattern: 'code_block',
@@ -65,8 +69,8 @@ const hlasmListingEndevorGrammar: GrammarProperties = {
   listingOffset: '.'
 }
 
-const hlasmListingEndevorLongGrammar: GrammarProperties = {
-  grammarDestination: './syntaxes/hlasmListingEndevorLong.tmLanguage.json',
+const hlasmListingEndevorLongGrammar: GrammarDetails = {
+  file: './syntaxes/hlasmListingEndevorLong.tmLanguage.json',
   grammarName: 'IBM HLASM Listing Endevor Long',
   scope: 'hlasmListingEndevorLong',
   entryPattern: 'code_block',
@@ -75,10 +79,33 @@ const hlasmListingEndevorLongGrammar: GrammarProperties = {
   listingOffset: '.'
 }
 
-function generate(props: GrammarProperties) {
+interface GrammarBase extends GrammarFile {
+  includedGrammars: GrammarDetails[];
+}
+
+const hlasmBase: GrammarBase = {
+  file: './syntaxes/hlasm.tmLanguage.json',
+  grammarName: 'IBM HLASM',
+  scope: 'hlasm',
+  includedGrammars: [hlasmGeneralGrammar]
+}
+
+const hlasmListingBase: GrammarBase = {
+  file: './syntaxes/hlasmListing.tmLanguage.json',
+  grammarName: 'IBM HLASM Listing',
+  scope: 'hlasmListing',
+  includedGrammars: [
+    hlasmListingGeneralGrammar,
+    hlasmListingGeneralLongGrammar,
+    hlasmListingEndevorGrammar,
+    hlasmListingEndevorLongGrammar
+  ]
+}
+
+function generateGrammarsDetails(props: GrammarDetails) {
   const listingDetails = props.listingOffset + props.beginLineSkipRule;
 
-  fs.readFile(grammar_template, 'utf8', (err: Error, data: string) => {
+  fs.readFile(grammar_details_template, 'utf8', (err: Error, data: string) => {
     if (err) {
       return console.log(err);
     }
@@ -90,14 +117,42 @@ function generate(props: GrammarProperties) {
     result = result.replaceAll('${listingOffset}$', props.listingOffset);
     result = result.replaceAll('${listingDetails}$', listingDetails);
 
-    fs.writeFile(props.grammarDestination, result, 'utf8', (err: any) => {
+    fs.writeFile(props.file, result, 'utf8', (err: any) => {
       if (err) return console.log(err);
     });
   });
 }
 
-generate(hlasmGrammar);
-generate(hlasmListingGrammar);
-generate(hlasmListingLongGrammar);
-generate(hlasmListingEndevorGrammar);
-generate(hlasmListingEndevorLongGrammar);
+function generateGrammarBase(props: GrammarBase) {
+  fs.readFile(grammar_base_template, 'utf8', (err: Error, data: string) => {
+    let includeRule = 'include: #{}';
+
+    if (err) {
+      return console.log(err);
+    }
+
+    if (props.includedGrammars.length !== 0) {
+      includeRule = '{"include": "source.' + props.includedGrammars[0].scope + '"}'
+
+      for (let i = 1; i < props.includedGrammars.length; ++i) 
+        includeRule += ',{"include": "source.' + props.includedGrammars[i].scope + '"}'
+    }
+
+    let result = data.replaceAll('${grammarName}$', props.grammarName);
+    result = result.replaceAll('${scope}$', props.scope);
+    result = result.replaceAll('${patterns}$', includeRule);
+
+    fs.writeFile(props.file, result, 'utf8', (err: any) => {
+      if (err) return console.log(err);
+    });
+  });
+}
+
+generateGrammarsDetails(hlasmGeneralGrammar);
+generateGrammarsDetails(hlasmListingGeneralGrammar);
+generateGrammarsDetails(hlasmListingGeneralLongGrammar);
+generateGrammarsDetails(hlasmListingEndevorGrammar);
+generateGrammarsDetails(hlasmListingEndevorLongGrammar);
+
+generateGrammarBase(hlasmBase);
+generateGrammarBase(hlasmListingBase);
